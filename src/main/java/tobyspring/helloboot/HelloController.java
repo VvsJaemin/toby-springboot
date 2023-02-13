188549e38ebf27c5cb20c0aaf9baa50952f3f763
VvsJaemin <djd453216@nate.com>
2023-02-13 22:43:49 +0900

    스프링 컨테이너 사용, DI, DispatcherServlet, 애노테이션 매핑

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,13 +1,25 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Objects;
+
+@RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/hello")
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
-        return "hello " + name;
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
