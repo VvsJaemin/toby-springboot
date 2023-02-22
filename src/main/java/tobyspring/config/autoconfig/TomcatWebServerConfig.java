7f27886a55b0a196a437cff585561006e33d3939
VvsJaemin <djd453216@nate.com>
2023-02-22 22:46:22 +0900

    자동 구성 정보 대체, @Conditional 정리

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
@@ -13,6 +14,7 @@ import tobyspring.config.MyAutoConfiguration;
 public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean //빈의 존재 여부 기준으로 포함여부 결정 -> 지정된 빈 정보가 없으면 메소드의 리턴 타입을 기준으로 빈의 존재여부 체크
     public ServletWebServerFactory serverFactory() {
         return new TomcatServletWebServerFactory();
     }
