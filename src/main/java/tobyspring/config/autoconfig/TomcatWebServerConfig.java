b6e23181ed178863234ff058b4b0f4f852aa6fc1
VvsJaemin <djd453216@nate.com>
2023-02-27 22:05:34 +0900

    외부 설정을 이용한 자동구성

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -4,19 +4,24 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 import tobyspring.config.ConditionalMyOnClass;
+import tobyspring.config.EnableMyConfigurationProperties;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat") // 추상화
+@EnableMyConfigurationProperties(ServerProperties.class)
 public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean //빈의 존재 여부 기준으로 포함여부 결정 -> 지정된 빈 정보가 없으면 메소드의 리턴 타입을 기준으로 빈의 존재여부 체크
-    public ServletWebServerFactory serverFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory serverFactory(ServerProperties properties) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
+
+        return factory;
     }
 
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,6 +1,9 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 import tobyspring.config.MySpringBootApplication;
 
 @MySpringBootApplication
